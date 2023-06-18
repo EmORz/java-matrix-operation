c966df47c9ce6de1f3abdb4f4b188fdc016d369f
Emanuil <eiv.razgrad@gmail.com>
2023-06-18 22:46:48 +0300

    123

--- Matrix_Operation/src/Main.java
+++ Matrix_Operation/src/Main.java
@@ -20,7 +20,7 @@ public class Main {
             }
         }
 
-        System.out.println("Matrix is: ");
+        System.out.println("Matrix is1: ");
         printMatrix(matrix);
 
         System.out.print("Sum of matrixs is: ");
